5f6ce4d7c796d2ab34f65e681ed82931cdd700d8
Eduard-Prinz <144338386+Eduard-Prinz@users.noreply.github.com>
2023-12-30 13:37:54 +0300

    Начало выполнения ДЗ №22

---
+++ src/main/java/mapDd/HW112/Hw112Application.java
@@ -0,0 +1,13 @@
+package mapDd.HW112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw112Application.class, args);
+	}
+
+}
---
+++ src/test/java/mapDd/HW112/Hw112ApplicationTests.java
@@ -0,0 +1,13 @@
+package mapDd.HW112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
